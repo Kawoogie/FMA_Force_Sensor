49fc22c0e5aef6e7f794e2264c3866bc6dad65c4
Laptop <ldsikstrom@gmail.com>
2023-06-14 15:15:55 -0400

    Initial Commit

---
+++ fma_force_sensor.cpp
@@ -0,0 +1,139 @@
+#include "fma_force_sensor.h"
+#include "mbed.h"
+
+
+// Count values for calculating the calibrated force value
+#define FORCE_MAX_COUNT                                    16384.0
+#define FORCE_90_COUNT                                     14745.6
+#define FORCE_80_COUNT                                     13107.2
+#define FORCE_20_COUNT                                     3276.8
+#define FORCE_10_COUNT                                     1638.4
+
+// Bit masks for decoding the data output
+#define FORCE5_STATES_BIT_MASK                             0x03
+
+
+#define CALIBRATION_DELAY                                  10ms
+
+// Default address for the sensor
+const int addr8bit = 0x28 << 1; // 8bit I2C address, 0x80
+
+
+/*****************************************************************************/
+// Constructor
+/*****************************************************************************/
+/*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*/
+FMA_Force_Sensor::FMA_Force_Sensor(
+                            PinName Data,
+                            PinName Clock,
+                            int max_value,
+                            int transfer_max
+                            ):
+                            sh_i2c(Data, Clock)
+    {
+        // Set up I2C communication
+
+        // Set values for max_value and transfer_max assignments
+        _max_range = max_value;
+        _transfer_value = transfer_max;
+        _device_address = addr8bit;
+        _zero_value = 0.0;
+        // Address of the sensor
+
+    }
+
+void FMA_Force_Sensor::get_force(float* force){
+    int force_raw;
+
+    _get_force_raw(force_raw);
+
+
+    // Calibrate the force value depending on the transfer function range.
+    if (_transfer_value == 80){
+        force = ((float(&force_raw) - FORCE_20_COUNT) / (FORCE_80_COUNT - FORCE_20_COUNT)) * _max_range;
+    }
+    else{
+        force = ((float(&force_raw) - FORCE_10_COUNT) / (FORCE_90_COUNT - FORCE_10_COUNT)) * _max_range;
+    }
+
+    // Remove the zero offset from the value
+    force = force - _zero_value;
+    
+}
+
+
+
+void FMA_Force_Sensor::_get_force_raw(int* force_raw){
+    char response[4];
+    int status;
+
+    _read_from_sensor(&response);
+
+    // Get the status of the force read
+    status = ( response[0] >> 6 ) & FORCE_STATE_BIT_MASK;
+
+    // Parse the force data from the response
+    force_raw = response[0];
+    force_raw <<= 8;
+    force_raw |= response[1];
+    force_raw &= 0x3FFF; // Apply mask
+
+}
+
+void FMA_Force_Sensor::_get_temp_raw(int* temp_raw){
+    char response[4];
+    int status;
+
+    _read_from_sensor(&response);
+
+    // Get the status of the force read
+    status = ( cmd[0] >> 6 ) & FORCE_STATE_BIT_MASK;
+
+    // Parse the temp data from the response
+    temp_raw = cmd[2];
+    temp_raw <<= 8;
+    temp_raw |= cmd[3];
+    temp_raw >>= 5;
+    temp_raw &= 0x07FF;
+
+
+}
+
+char[4] FMA_Force_Sensor::_read_from_sensor(void){
+    char response[4];
+
+    i2c.read(_device_address, response, 4);
+
+    return response;
+
+}
+
+
+void FMA_Force_Sensor::set_address(int new_address){
+    _device_address = new_address;
+}
+
+void FMA_Force_Sensor::set_zero(void){
+    // uint16_t force_val;
+    // uint8_t status;
+    uint8_t n_cnt;
+    float sum;
+    float force = 0.0;
+    
+    sum = 0;
+    
+    for ( n_cnt = 0; n_cnt < 10; n_cnt++ ) {
+        get_force(force);
+        
+        sum += force;
+        
+        _calibration_delay();
+    }
+    
+    _zero_value = ( sum / 10.0 );
+}
+
+
+void FMA_Force_Sensor::_calibration_delay (void) {
+    ThisThread::sleep_for(CALIBRATION_DELAY);
+}
---
+++ fma_force_sensor.h
@@ -0,0 +1,78 @@
+#ifndef FMA_FORCE_SENSOR_H
+#define FMA_FORCE_SENSOR_H
+/**
+ *  Library to get force and temperature readings from the FMA series of
+ *  micro force sensors from Honeywell.
+ * 
+ *  This code is written to get data from the I2C protocol, not the SPI protocl
+ *  
+ *  This was written for use with Mbed OS 6.16.0 on a MAX32630FTHR
+ * 
+ * Example of use:
+ * @code
+ * #include "mbed.h"
+ * #include "max32664.h"
+ * #include "USBSerial.h"  // For communication via the onboard USB port
+ * 
+ * 
+ * // Start serial communication
+ * USBSerial serial;
+ * 
+ * // Create the Sensor_Raw object
+ * FMA_Force_Sensor force_sensor;
+ * 
+ * int main()
+ * {   
+ *     // Initialize the sensor hub with default values
+ *     sensor_hub.init_sh_raw();
+ *     
+ *     // Set the data interrogation rate to 50 Hz.
+ *     sensor_hub.set_sample_rate(50);
+ *     
+ *     // Set the drive current for all 3 LEDs 
+ *     sensor_hub.set_led_current(0x12);
+ *  
+ *     // Variables to hold the ppg data
+ *     int red_ppg, green_ppg, ir_ppg;
+ * 
+ *     while (true) {
+ *       // Read the data at a rate of 50 Hz and output the information via serial
+ *       sensor_hub.read_sh_fifo(&red_ppg, &green_ppg, &ir_ppg);
+ *       serial.printf("%d,%d,%d,\n\r", red_ppg, green_ppg, ir_ppg);
+ *       ThisThread::sleep_for(20ms);
+    }
+ * }
+ * @endcode
+ * 
+*/
+#include "mbed.h"
+
+class FMA_Force_Sensor {
+    public:
+    /** Constructor
+     * @param Data data pin for the I2C communication
+     * @param Clock clock pin for the I2C communication
+     * @param max_value max range of force reading value
+     * @param transfer_max upper limit of transfer function percentage.
+    */
+    FMA_Force_Sensor(PinName Data = P5_7, PinName Clock = P6_0, int max_value = 25, int transfer_max = 80);
+    void get_force(float* force);
+    void get_temp(float* temperature);
+    void set_zero(void);
+    void set_address(int new_address);
+    
+
+    private:
+    I2C sh_i2c;
+    int _max_range;
+    int _transfer_value;
+    int _device_address;
+    int _zero_value;
+    void _get_force_raw(int* force_raw);
+    void _get_temp_raw(int* temp_raw);
+    char[4] _read_from_sensor(void);
+    void _calibration_delay(void);
+
+};
+
+#endif
